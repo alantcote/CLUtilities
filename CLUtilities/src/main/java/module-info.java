97d968fecc7cff990cd51111660149c7b3610c66
Alan T. Cote' <alantcote@gmail.com>
2021-12-25 07:18:22 -0500

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/alantcote/CLUtilities.git.
    Playing catch-up.
    
    Now playing along with the modules thing.

---
+++ CLUtilities/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module clutilities {
+	exports net.sf.cotelab.util.swing.tree;
+	exports net.sf.cotelab.util.math;
+	exports net.sf.cotelab.util.collections;
+	exports net.sf.cotelab.util.io;
+	exports net.sf.cotelab.util;
+
+	requires java.desktop;
+	requires java.logging;
+//	requires junit;
+}
