f9cbe806db35aec353da5ba82a6d593d6c32303e
Alan T. Cote' <alantcote@gmail.com>
2021-12-28 18:57:33 -0500

    Modularized the library. Renovated pom.

--- CLUtilities/src/main/java/module-info.java
+++ CLUtilities/src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module util {
 	exports net.sf.cotelab.util;
-
+	exports net.sf.cotelab.util.collections;
+	exports net.sf.cotelab.util.io;
+	exports net.sf.cotelab.util.math;
+	exports net.sf.cotelab.util.swing.tree;
+	
 	requires java.desktop;
 	requires java.logging;
 }
