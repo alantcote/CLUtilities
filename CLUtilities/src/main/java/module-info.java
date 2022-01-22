0ad517da8d3128a2afbd6777dc6867ba9a5697bd
Alan T. Coté <alantcote@gmail.com>
2022-01-22 11:45:35 -0500

    Copied MetricListener and WindowPrefs from dupfilefinder2.

--- CLUtilities/src/main/java/module-info.java
+++ CLUtilities/src/main/java/module-info.java
@@ -2,11 +2,13 @@ module clutilities {
 	exports cotelab.util.demo;
 	exports cotelab.util.javafx.scene.control;
 	exports cotelab.util.javafx.util.callback;
+	exports cotelab.util.javafx.windowprefs;
 	
 	requires transitive java.desktop;
 	requires java.logging;
-	requires transitive javafx.graphics;
-	requires javafx.controls;
-	requires javafx.swing;
+	requires transitive java.prefs;
 	requires javafx.base;
+	requires javafx.controls;
+	requires transitive javafx.graphics;
+	requires javafx.swing;
 }
