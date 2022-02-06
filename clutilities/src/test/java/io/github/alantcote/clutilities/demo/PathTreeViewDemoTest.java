ff8a4c4be556c93f3d208bcc54eb31c627616bba
Alan T. Coté <96086810+alantcote@users.noreply.github.com>
2022-02-06 11:44:12 -0500

    Add unit test case for AlertsDemo.

--- clutilities/src/test/java/io/github/alantcote/clutilities/demo/PathTreeViewDemoTest.java
+++ clutilities/src/test/java/io/github/alantcote/clutilities/demo/PathTreeViewDemoTest.java
@@ -24,7 +24,37 @@ import javafx.stage.Stage;
 public class PathTreeViewDemoTest {
 
 	/**
-	 * Test method for {@link io.github.alantcote.clutilities.demo.PathTreeViewDemo#main(java.lang.String[])}.
+	 * Test method for
+	 * {@link io.github.alantcote.clutilities.demo.PathTreeViewDemo#createPathTreeView()}.
+	 */
+	@Test
+	public void testCreatePathTreeView() {
+		final PathTreeItem pathTreeItem = new PathTreeItem(null);
+		final PathTreeView pathTreeView = new PathTreeView(pathTreeItem);
+		PathTreeViewDemo fixture = new PathTreeViewDemo() {
+
+			@Override
+			protected PathTreeItem newPathTreeItem() {
+				return pathTreeItem;
+			}
+
+			@Override
+			protected PathTreeView newPathTreeView(TreeItem<Path> rootPathTreeItem) {
+				assertTrue(pathTreeItem == rootPathTreeItem);
+
+				return pathTreeView;
+			}
+
+		};
+
+		TreeView<Path> result = fixture.createPathTreeView();
+
+		assertEquals(pathTreeView, result);
+	}
+
+	/**
+	 * Test method for
+	 * {@link io.github.alantcote.clutilities.demo.PathTreeViewDemo#main(java.lang.String[])}.
 	 */
 	@Test
 	public void testMain() {
@@ -33,7 +63,141 @@ public class PathTreeViewDemoTest {
 	}
 
 	/**
-	 * Test method for {@link io.github.alantcote.clutilities.demo.PathTreeViewDemo#start(javafx.stage.Stage)}.
+	 * Test method for
+	 * {@link io.github.alantcote.clutilities.demo.PathTreeViewDemo#newBorderPane()}.
+	 */
+	@Test
+	public void testNewBorderPane() {
+		final PathTreeViewDemo fixture = new PathTreeViewDemo();
+
+		assertNotNull(fixture.newBorderPane());
+	}
+
+	/**
+	 * Test method for
+	 * {@link io.github.alantcote.clutilities.demo.PathTreeViewDemo#newPathTreeItem()}.
+	 */
+	@Test
+	public void testNewPathTreeItem() {
+		final PathTreeViewDemo fixture = new PathTreeViewDemo();
+
+		assertNotNull(fixture.newPathTreeItem());
+	}
+
+	/**
+	 * Test method for
+	 * {@link io.github.alantcote.clutilities.demo.PathTreeViewDemo#newPathTreeView(javafx.scene.control.TreeItem)}.
+	 */
+	@Test
+	public void testNewPathTreeView() {
+		final PathTreeViewDemo fixture = new PathTreeViewDemo();
+		final TreeItem<Path> pathTreeItem = new PathTreeItem(null);
+
+		assertNotNull(fixture.newPathTreeView(pathTreeItem));
+	}
+
+	/**
+	 * Test method for
+	 * {@link io.github.alantcote.clutilities.demo.PathTreeViewDemo#newScene(javafx.scene.layout.BorderPane)}.
+	 */
+	@Test
+	public void testNewScene() {
+		final PathTreeViewDemo fixture = new PathTreeViewDemo();
+		final BorderPane borderPane = new BorderPane();
+
+		assertNotNull(fixture.newScene(borderPane));
+	}
+
+	/**
+	 * Test method for
+	 * {@link io.github.alantcote.clutilities.demo.PathTreeViewDemo#populatePathTreeViewPane(javafx.scene.layout.BorderPane)}.
+	 */
+	@Test
+	public void testPopulatePathTreeViewPane() {
+		final PathTreeItem pathTreeItem = new PathTreeItem(null);
+		final TreeView<Path> treeView = new PathTreeView(pathTreeItem);
+		final BorderPane borderPane = new BorderPane();
+		final PathTreeViewDemo fixture = new PathTreeViewDemo() {
+
+			@Override
+			protected TreeView<Path> createPathTreeView() {
+				return treeView;
+			}
+
+		};
+
+		fixture.populatePathTreeViewPane(borderPane);
+
+		assertTrue(treeView == borderPane.getCenter());
+	}
+
+	/**
+	 * Test method for
+	 * {@link io.github.alantcote.clutilities.demo.PathTreeViewDemo#stageSetOnShown(javafx.stage.Stage)}.
+	 */
+	@Test
+	@TestInJfxThread
+	public void testStageSetOnShown() {
+		final PathTreeViewDemo fixture = new PathTreeViewDemo();
+		Stage stage = new Stage();
+
+		fixture.stageSetOnShown(stage);
+
+		assertNotNull(stage.getOnShown());
+	}
+
+	/**
+	 * Test method for
+	 * {@link io.github.alantcote.clutilities.demo.PathTreeViewDemo#stageSetScene(javafx.stage.Stage, javafx.scene.Scene)}.
+	 */
+	@Test
+	@TestInJfxThread
+	public void testStageSetScene() {
+		final PathTreeViewDemo fixture = new PathTreeViewDemo();
+		Stage stage = new Stage();
+		BorderPane borderPane = new BorderPane();
+		Scene scene = new Scene(borderPane);
+
+		fixture.stageSetScene(stage, scene);
+
+		assertTrue(scene == stage.getScene());
+	}
+
+	/**
+	 * Test method for
+	 * {@link io.github.alantcote.clutilities.demo.PathTreeViewDemo#stageSetTitle(javafx.stage.Stage)}.
+	 */
+	@Test
+	@TestInJfxThread
+	public void testStageSetTitle() {
+		final PathTreeViewDemo fixture = new PathTreeViewDemo();
+		Stage stage = new Stage();
+
+		fixture.stageSetTitle(stage);
+
+		assertEquals("Path TreeView Demo", stage.getTitle());
+	}
+
+	/**
+	 * Test method for
+	 * {@link io.github.alantcote.clutilities.demo.PathTreeViewDemo#stageShow(javafx.stage.Stage)}.
+	 */
+	@Test
+	@TestInJfxThread
+	public void testStageShow() {
+		// the solution in the comments below passes, but results in opening a
+		// window during test. commented out because it's not cool to pop up UI
+		// artifacts during a unit test and the stageShow() method is so trivial
+		// that it's hard to imagine a way it could go wrong.
+//		final PathTreeViewDemo fixture = new PathTreeViewDemo();
+//		Stage stage = new Stage();
+//		
+//		fixture.stageShow(stage);
+	}
+
+	/**
+	 * Test method for
+	 * {@link io.github.alantcote.clutilities.demo.PathTreeViewDemo#start(javafx.stage.Stage)}.
 	 */
 	@Test
 	@TestInJfxThread
@@ -51,7 +215,7 @@ public class PathTreeViewDemoTest {
 			@Override
 			protected Scene newScene(BorderPane root) {
 				assertTrue(borderPane == root);
-				
+
 				return scene;
 			}
 
@@ -84,158 +248,6 @@ public class PathTreeViewDemoTest {
 		};
 
 		fixture.start(stage);
-	}
-
-	/**
-	 * Test method for {@link io.github.alantcote.clutilities.demo.PathTreeViewDemo#createPathTreeView()}.
-	 */
-	@Test
-	public void testCreatePathTreeView() {
-		final PathTreeItem pathTreeItem = new PathTreeItem(null);
-		final PathTreeView pathTreeView = new PathTreeView(pathTreeItem);
-		PathTreeViewDemo fixture = new PathTreeViewDemo() {
-
-			@Override
-			protected PathTreeItem newPathTreeItem() {
-				return pathTreeItem;
-			}
-
-			@Override
-			protected PathTreeView newPathTreeView(TreeItem<Path> rootPathTreeItem) {
-				assertTrue(pathTreeItem == rootPathTreeItem);
-				
-				return pathTreeView;
-			}
-
-		};
-
-		TreeView<Path> result = fixture.createPathTreeView();
-
-		assertEquals(pathTreeView, result);
-	}
-
-	/**
-	 * Test method for {@link io.github.alantcote.clutilities.demo.PathTreeViewDemo#newBorderPane()}.
-	 */
-	@Test
-	public void testNewBorderPane() {
-		final PathTreeViewDemo fixture = new PathTreeViewDemo();
-
-		assertNotNull(fixture.newBorderPane());
-	}
-
-	/**
-	 * Test method for {@link io.github.alantcote.clutilities.demo.PathTreeViewDemo#newPathTreeItem()}.
-	 */
-	@Test
-	public void testNewPathTreeItem() {
-		final PathTreeViewDemo fixture = new PathTreeViewDemo();
-
-		assertNotNull(fixture.newPathTreeItem());
-	}
-
-	/**
-	 * Test method for {@link io.github.alantcote.clutilities.demo.PathTreeViewDemo#newPathTreeView(javafx.scene.control.TreeItem)}.
-	 */
-	@Test
-	public void testNewPathTreeView() {
-		final PathTreeViewDemo fixture = new PathTreeViewDemo();
-		final TreeItem<Path> pathTreeItem = new PathTreeItem(null);
-
-		assertNotNull(fixture.newPathTreeView(pathTreeItem));
-	}
-
-	/**
-	 * Test method for {@link io.github.alantcote.clutilities.demo.PathTreeViewDemo#newScene(javafx.scene.layout.BorderPane)}.
-	 */
-	@Test
-	public void testNewScene() {
-		final PathTreeViewDemo fixture = new PathTreeViewDemo();
-		final BorderPane borderPane = new BorderPane();
-
-		assertNotNull(fixture.newScene(borderPane));
-	}
-
-	/**
-	 * Test method for {@link io.github.alantcote.clutilities.demo.PathTreeViewDemo#populatePathTreeViewPane(javafx.scene.layout.BorderPane)}.
-	 */
-	@Test
-	public void testPopulatePathTreeViewPane() {
-		final PathTreeItem pathTreeItem = new PathTreeItem(null);
-		final TreeView<Path> treeView = new PathTreeView(pathTreeItem);
-		final BorderPane borderPane = new BorderPane();
-		final PathTreeViewDemo fixture = new PathTreeViewDemo() {
-
-			@Override
-			protected TreeView<Path> createPathTreeView() {
-				return treeView;
-			}
-
-		};
-
-		fixture.populatePathTreeViewPane(borderPane);
-		
-		assertTrue(treeView == borderPane.getCenter());
-	}
-
-	/**
-	 * Test method for {@link io.github.alantcote.clutilities.demo.PathTreeViewDemo#stageSetOnShown(javafx.stage.Stage)}.
-	 */
-	@Test
-	@TestInJfxThread
-	public void testStageSetOnShown() {
-		final PathTreeViewDemo fixture = new PathTreeViewDemo();
-		Stage stage = new Stage();
-		
-		fixture.stageSetOnShown(stage);
-		
-		assertNotNull(stage.getOnShown());
-	}
-
-	/**
-	 * Test method for {@link io.github.alantcote.clutilities.demo.PathTreeViewDemo#stageSetScene(javafx.stage.Stage, javafx.scene.Scene)}.
-	 */
-	@Test
-	@TestInJfxThread
-	public void testStageSetScene() {
-		final PathTreeViewDemo fixture = new PathTreeViewDemo();
-		Stage stage = new Stage();
-		BorderPane borderPane = new BorderPane();
-		Scene scene = new Scene(borderPane);
-		
-		fixture.stageSetScene(stage, scene);
-		
-		assertTrue(scene == stage.getScene());
-	}
-
-	/**
-	 * Test method for {@link io.github.alantcote.clutilities.demo.PathTreeViewDemo#stageSetTitle(javafx.stage.Stage)}.
-	 */
-	@Test
-	@TestInJfxThread
-	public void testStageSetTitle() {
-		final PathTreeViewDemo fixture = new PathTreeViewDemo();
-		Stage stage = new Stage();
-		
-		fixture.stageSetTitle(stage);
-		
-		assertEquals("Path TreeView Demo", stage.getTitle());
-	}
-
-	/**
-	 * Test method for {@link io.github.alantcote.clutilities.demo.PathTreeViewDemo#stageShow(javafx.stage.Stage)}.
-	 */
-	@Test
-	@TestInJfxThread
-	public void testStageShow() {
-		// the solution in the comments below passes, but results in opening a
-		// window during test. commented out because it's not cool to pop up UI
-		// artifacts during a unit test and the stageShow() method is so trivial
-		// that it's hard to imagine a way it could go wrong.
-//		final PathTreeViewDemo fixture = new PathTreeViewDemo();
-//		Stage stage = new Stage();
-//		
-//		fixture.stageShow(stage);
 	}
 
 }
