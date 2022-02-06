3bc4262b3c07793b3b99cd0afcf82f3fea5af9a6
Alan T. Coté <96086810+alantcote@users.noreply.github.com>
2022-02-06 09:05:23 -0500

    Delete CLUtilities directory

--- CLUtilities/README.md
+++
@@ -1,3 +0,0 @@
-# CLUtilities
-
-Simple (or not so much) utility classes.
--- CLUtilities/src/main/java/io/github/alantcote/clutilities/javafx/util/callback/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Extensions of classes in the JavaFX <code>javafx.util.callback}</code>
- * package.
- * 
- * Some classes in this package are here to provide support.
- */
-package io.github.alantcote.clutilities.javafx.util.callback;
